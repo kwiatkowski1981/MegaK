13bf02fd42c9af9443f2b496a35b0e1f6669e65f
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-07-14 17:10:39 +0200

    added basic exercises with async/await setTimeout / setInterval

---
+++ week_3/exercise4.js
@@ -0,0 +1,23 @@
+const {setTimeout} = require('timers/promises');
+
+async function brewTea() {
+    console.log('Brewing Tea...');
+    await setTimeout(2000);
+    console.log('Tea is ready!!!');
+}
+
+async function orderKeyboard() {
+    console.log('Ordering and waiting for package with keyboard...');
+    await setTimeout(2000);
+    console.log('Keyboard has arrived!!!');
+}
+
+(async () => {
+    // await brewTea();
+    // await orderKeyboard();
+    // jak chce żeby wszystkie promisy zrobily sie jednoczesnie to robie tak:
+    // przekazuje tablice wszystkich promisow:
+    await Promise.all([brewTea(), orderKeyboard()]);
+    await setTimeout(500);
+    console.log('Finished.');
+})();
